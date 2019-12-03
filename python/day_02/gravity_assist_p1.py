f258ffc6833b51a4b8cf1c34bce1f711f53b1a1a
William DeLuca <wdeluca@direwolfdigital.com>
2019-12-03 23:57:29 +0000

    Day 2 Part 1 Challenge

---
+++ python/day_02/gravity_assist_p1.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+
+def load_gravity_assist_program(filename):
+    file = open(filename, 'r').read().strip("\n").split(",")      # Read the File
+    return [int(line) for line in file]                           # List of INT's
+
+def main():
+
+    gravity_assist_program = 'gravity_assist_input.txt'
+
+    module_masses = load_gravity_assist_program(gravity_assist_program)
+
+    module_masses[1] = 12   # Per Challenge this value is set
+    module_masses[2] = 2    # Per Challenge this value is set
+
+    for i, compute_value in enumerate(module_masses):
+        if i % 4 == 0:      # Shifting by 4 for Operator Value
+
+            if compute_value == 99: # The 99 Operator Value signals HAULT
+                break
+
+            action, value_one_pos, value_two_pos, store_pos, *_ = module_masses[i:-1]   # Set Required Positions
+
+            if  compute_value == 1:     # Operator 1 is addition of the provided positions
+                module_masses[store_pos] = (module_masses[value_one_pos] + module_masses[value_two_pos])
+            elif compute_value == 2:    # Operator 1 is multiplication of the provided positions
+                module_masses[store_pos] = (module_masses[value_one_pos] * module_masses[value_two_pos])
+            else:                       # Values other than [99, 1, 2] are invalid
+                print("Unknown Operator Value:", compute_value)
+                break
+
+            
+    print("1202 Alarm State is:", module_masses[0]) 
+
+if __name__ == "__main__":
+    main()
