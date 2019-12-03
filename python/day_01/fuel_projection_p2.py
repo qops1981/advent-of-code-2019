454bea91167f5e8c9c252c9d747f2db519717933
William DeLuca <wdeluca@direwolfdigital.com>
2019-12-03 16:26:50 +0000

    Day 1 Part 2 Challenge, Impliment Fuel Recursion Calculation

--- python/day_01/fuel_projection_p2.py
+++ python/day_01/fuel_projection_p2.py
@@ -1,12 +1,15 @@
 #! /usr/bin/env python3
 
 def load_module_mass_index(filename):
-    file = open(filename, 'r').readlines()              # Read the File Lines
-    return [int(line.rstrip('\n')) for line in file]    # List of Lines without newline and as INT
+    file = open(filename, 'r').readlines()                  # Read the File Lines
+    return [int(line.rstrip('\n')) for line in file]        # List of Lines without newline and as INT
 
 def calculate_fuel_for_mass(mass):
-    fuel 
-    return int(( mass / 3 ) - 2)    # Fuel Calculation Per the Challenge
+    calculation = ( ( mass / 3 ) - 2 )
+    if calculation <= 0:
+        return 0
+    else:
+        return int(calculation + calculate_fuel_for_mass(calculation))    # Fuel Calculation Per the Challenge
 
 def main():
 
